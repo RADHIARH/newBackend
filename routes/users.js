90ef530abf512d32efae7faba401f4f40a38fefa
RADHIARH <radhia.rahmani@sesame.com.tn>
2022-06-14 12:45:26 +0200

    send to git

--- routes/users.js
+++ routes/users.js
@@ -2,5 +2,5 @@ const controller = require("../controllers/controller");
 const express = require("express");
 const router = express.Router();
 const middleware = require("../middleware/middleware");
-router.get("/", controller.getusers);
+router.get("/", middleware.verifyToken, controller.getusers);
 module.exports = router;
